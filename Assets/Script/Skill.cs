34ee8ef30dd91a2d98508552e25f7668d163a161
kanzzzzz <thsalstn012@naver.com>
2026-05-07 09:54:05 +0900

    05/07

--- Assets/Script/Skill.cs
+++ Assets/Script/Skill.cs
@@ -1,14 +1,13 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static SkillList;
 
 public class Skill : MonoBehaviour
 {
     public Image skillImage;
     public TextMeshProUGUI skillNameText;
     public TextMeshProUGUI skillText;
-
-    private Image buttonSprite;
 
     public SkillList skillList;
 
@@ -19,7 +18,13 @@ public class Skill : MonoBehaviour
 
     void Start()
     {
-        buttonSprite = gameObject.GetComponent<Image>();
+        skillList.TryGetSkill("TetrisBlock", out SkillList.SkillInfo basicSkillInfo);
+
+        skillImage.sprite = basicSkillInfo.sprite;
+        skillNameText.text = basicSkillInfo.skillName;
+        skillText.text = basicSkillInfo.description;
+
+        SelectedSkillData.Set(basicSkillInfo.sprite, basicSkillInfo.skillName, basicSkillInfo.description);
     }
 
     
@@ -33,10 +38,10 @@ public class Skill : MonoBehaviour
         }
 
 
-        skillImage.sprite = buttonSprite.sprite;
+        skillImage.sprite = skillInfo.sprite;
         skillNameText.text = skillInfo.skillName;
         skillText.text = skillInfo.description;
 
-        SelectedSkillData.Set(buttonSprite.sprite, skillInfo.skillName, skillInfo.description);
+        SelectedSkillData.Set(skillInfo.sprite, skillInfo.skillName, skillInfo.description);
     }
 }
